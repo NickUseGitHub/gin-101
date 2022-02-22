165b82b9b397df97c3c38cd8d7019e4a1f13dc78
Chaiwat Tungtongsoontorn <chaiwat_t@tripetch-it.co.th>
2022-02-22 23:00:20 +0700

    feat: module book's CRUD

--- src/modules/book/api.go
+++ src/modules/book/api.go
@@ -4,4 +4,8 @@ import "github.com/gin-gonic/gin"
 
 func RegisterController(r *gin.Engine) {
 	r.GET("/books", FindBooks)
+	r.GET("/books/:id", FindBook)
+	r.POST("/books", CreateBook)
+	r.PATCH("/books/:id", UpdateBook)
+	r.DELETE("/books/:id", DeleteBook)
 }
--- src/modules/book/controller.go
+++ src/modules/book/controller.go
@@ -7,6 +7,22 @@ import (
 	"github.com/jinzhu/gorm"
 )
 
+func FindBook(c *gin.Context) { // Get model if exist
+	var book Book
+
+	db, ok := c.Keys["DB"].(*gorm.DB)
+	if !ok {
+		panic("DB is not called on *gin.Context")
+	}
+
+	if err := db.Where("id = ?", c.Param("id")).First(&book).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Record not found!"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"data": book})
+}
+
 func FindBooks(c *gin.Context) {
 	var books []Book
 
@@ -18,4 +34,68 @@ func FindBooks(c *gin.Context) {
 	db.Find(&books)
 
 	c.JSON(http.StatusOK, gin.H{"data": books})
+}
+
+func CreateBook(c *gin.Context) {
+	var input CreateBookInput
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	db, ok := c.Keys["DB"].(*gorm.DB)
+	if !ok {
+		panic("DB is not called on *gin.Context")
+	}
+
+	// Create book
+	book := Book{Title: input.Title, Author: input.Author}
+	db.Create(&book)
+
+	c.JSON(http.StatusOK, gin.H{"data": book})
+}
+
+func UpdateBook(c *gin.Context) {
+	// Get model if exist
+	var book Book
+
+	db, ok := c.Keys["DB"].(*gorm.DB)
+	if !ok {
+		panic("DB is not called on *gin.Context")
+	}
+
+	if err := db.Where("id = ?", c.Param("id")).First(&book).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Record not found!"})
+		return
+	}
+
+	// Validate input
+	var input UpdateBookInput
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	db.Model(&book).Updates(input)
+
+	c.JSON(http.StatusOK, gin.H{"data": book})
+}
+
+func DeleteBook(c *gin.Context) {
+	// Get model if exist
+	var book Book
+
+	db, ok := c.Keys["DB"].(*gorm.DB)
+	if !ok {
+		panic("DB is not called on *gin.Context")
+	}
+
+	if err := db.Where("id = ?", c.Param("id")).First(&book).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Record not found!"})
+		return
+	}
+
+	db.Delete(&book)
+
+	c.JSON(http.StatusOK, gin.H{"data": true})
 }
